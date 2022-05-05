a3e52ac5450daabd00dfbfb0471fd053d08c5ea7
mhrayhan <mhrayhan430@gmial.com>
2022-05-05 14:23:22 +0530

    server side initialized

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+require('dotenv').config();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Inventory Server is running')
+})
+
+app.listen(port, ()=> {
+    console.log(`inventory server is running with PORT:${port}`);
+})
